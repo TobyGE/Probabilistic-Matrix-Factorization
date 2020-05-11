c0297754ea66bb137c9bb057ca422bbdaf84f3fe
Yingqiang Ge <gyq1101@gmail.com>
2020-05-11 01:32:40 -0400

    get embeddings

--- RunExample.py
+++ RunExample.py
@@ -7,12 +7,13 @@ from ProbabilisticMatrixFactorization import PMF
 if __name__ == "__main__":
     file_path = "data/ml-100k/u.data"
     pmf = PMF()
-    pmf.set_params({"num_feat": 10, "epsilon": 1, "_lambda": 0.1, "momentum": 0.8, "maxepoch": 10, "num_batches": 100,
+    pmf.set_params({"num_feat": 10, "epsilon": 1, "_lambda": 0.1, "momentum": 0.8, "maxepoch": 20, "num_batches": 100,
                     "batch_size": 1000})
     ratings = load_rating_data(file_path)
     print(len(np.unique(ratings[:, 0])), len(np.unique(ratings[:, 1])), pmf.num_feat)
     train, test = train_test_split(ratings, test_size=0.2)  # spilt_rating_dat(ratings)
     pmf.fit(train, test)
+
 
     # Check performance by plotting train and test errors
     plt.plot(range(pmf.maxepoch), pmf.rmse_train, marker='o', label='Training Data')
